c87ef4f36e58c10465c63e91b52a58c772480d19
Manny <manny-apsel@hotmail.com>
2019-09-13 09:21:35 +0200

    fixed async function

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
-function returnUrlBasedOnIdOrName(input) {
-    var url = "test";
-    input = input.toLowerCase();
-    fetch("https://pokeapi.co/api/v2/pokemon/?&limit=964")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            console.log(data.results);
-            console.log(data.results[parseInt(input)-1]);
-            console.log(data.results[parseInt(input)-1].url);
-            let regex = new RegExp(/\d{1,3}/);
-            // als input nummer tussen 1-3 karakters is dan kun je meteen op zoek gaan naar de input
-            if (regex.test(input) && parseInt(input) < 807) {
-                url = data.results[parseInt(input)-1].url;
-            } else {
-                var arrayPokemon = data.results;
-                //kan geen foreach doen op array van JSON file dus plaats ge alles in array
-                arrayPokemon.forEach(function (pokemon) {
-                    if (pokemon.name === input) {
-                        url = pokemon.url;
-                    }
-                });
+async function fetchPokemonsList() {
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/?&limit=964");
+    return await response.json();
+}
+
+function returnUrlBasedOnIdOrName(input, data) {
+    console.log(data);
+    console.log(data.results);
+    console.log(data.results[parseInt(input) - 1]);
+    console.log(data.results[parseInt(input) - 1].url);
+    let regex = new RegExp(/\d{1,3}/);
+    // als input nummer tussen 1-3 karakters is dan kun je meteen op zoek gaan naar de input
+    if (regex.test(input) && parseInt(input) < 807) {
+        url = data.results[parseInt(input) - 1].url;
+    } else {
+        var arrayPokemon = data.results;
+        //kan geen foreach doen op array van JSON file dus plaats ge alles in array
+        arrayPokemon.forEach(function (pokemon) {
+            if (pokemon.name === input) {
+                url = pokemon.url;
             }
-            console.log(url);
-            return url;
         });
+    }
     console.log(url);
+    return url;
+}
+
+async function fetchPokemonData(url) {
+    let response = await fetch(url);
+    return await response.json();
+}
+
+function showPokemonData(data) {
+    console.log(data);
+    var pokemonId = data.id;
+    var pokemonName = data.name;
+    document.getElementById("ID-name").innerText = pokemonName;
+    var pokemonImgFront = data.sprites.front_default;
+    var pokemonImgBack = data.sprites.back_default;
+    var pokemonAlt = `No picture found of ${pokemonName}`;
+    var idBackImage = document.getElementById("back");
+    idBackImage.setAttribute("src", pokemonImgBack);
+    idBackImage.setAttribute("alt", pokemonAlt);
+    var idFrontImage = document.getElementById("front");
+    idFrontImage.setAttribute("src", pokemonImgFront);
+    idFrontImage.setAttribute("alt", pokemonAlt);
+    var pokemonAllMoves = Array.from(data.moves);
+    var pokemonFourMoves = chooseFourMoves(pokemonAllMoves);
+    var pokemonFourMovesName = getNamesMovesArray(pokemonFourMoves);
+    var classMoves = Array.from(document.getElementsByClassName("moves"));
+    console.log(pokemonFourMovesName);
+    pokemonFourMovesName.forEach(function (move, index) {
+        console.log(index, move);
+        document.getElementsByClassName("moves")[index].innerHTML = move;
+    });
+    return data.species.url;
 }
 
 function chooseFourMoves(arrayAllMoves) {
@@ -82,35 +109,12 @@ function fetchEvolutionChain(url) {
         })
 }
 
-function fetchPokemon(url) {
+/*function fetchPokemon(url) {
     fetch(url)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            var pokemonId = data.id;
-            var pokemonName = data.name;
-            document.getElementById("ID-name").innerText = pokemonName;
-            var pokemonImgFront = data.sprites.front_default;
-            var pokemonImgBack = data.sprites.back_default;
-            var pokemonAlt = `No picture found of ${pokemonName}`;
-            var idBackImage = document.getElementById("back");
-            idBackImage.setAttribute("src", pokemonImgBack);
-            idBackImage.setAttribute("alt", pokemonAlt);
-            var idFrontImage = document.getElementById("front");
-            idFrontImage.setAttribute("src", pokemonImgFront);
-            idFrontImage.setAttribute("alt", pokemonAlt);
-            var pokemonAllMoves = Array.from(data.moves);
-            var pokemonFourMoves = chooseFourMoves(pokemonAllMoves);
-            var pokemonFourMovesName = getNamesMovesArray(pokemonFourMoves);
-            var classMoves = Array.from(document.getElementsByClassName("moves"));
-            console.log(pokemonFourMovesName);
-            pokemonFourMovesName.forEach(function (move, index) {
-                console.log(index, move);
-                document.getElementsByClassName("moves")[index].innerHTML = move;
-            });
-            var urlSpecies = data.species.url;
             document.getElementById("prevev").addEventListener("click", function () {
                 fetchSpecies(urlSpecies);
             });
@@ -125,38 +129,21 @@ function fetchPokemon(url) {
             })
 
         });
-}
+}*/
 
 document.getElementById("go").addEventListener("click", function () {
     var input = document.getElementById("input").value;
-    var urlPokemon = "";
-    input = input.toLowerCase();
-    fetch("https://pokeapi.co/api/v2/pokemon/?&limit=964")
-        .then(function (response) {
-            return response.json();
-        })
+    fetchPokemonsList()
         .then(function (data) {
-            console.log(data);
-            console.log(data.results);
-            console.log(data.results[parseInt(input)-1]);
-            console.log(data.results[parseInt(input)-1].url);
-            let regex = new RegExp(/\d{1,3}/);
-            // als input nummer tussen 1-3 karakters is dan kun je meteen op zoek gaan naar de input
-            if (regex.test(input) && parseInt(input) < 807) {
-                urlPokemon = data.results[parseInt(input)-1].url;
-            } else {
-                var arrayPokemon = data.results;
-                //kan geen foreach doen op array van JSON file dus plaats ge alles in array
-                arrayPokemon.forEach(function (pokemon) {
-                    if (pokemon.name === input) {
-                        urlPokemon = pokemon.url;
-                    }
-                });
-            }
+            let urlPokemon = returnUrlBasedOnIdOrName(input, data);
+            fetchPokemonData(urlPokemon)
+                .then(function (data) {
+                    var urlSpecies = showPokemonData(data);
+                })
         });
-    fetchPokemon(urlPokemon);
-});
 
+
+});
 
 /*
 
